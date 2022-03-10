2b6d977b55ac8a217f67e52105bf4cab0a493899
Emmanuel Gumede <gumede.em@gmail.com>
2022-03-10 03:30:37 +0200

    budget app home page

---
+++ budget-app/src/components/Header.js
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <h2>Misen Personal Budget</h2>
+      </div>
+      <div></div>
+      <div className="log-in-btn">LOG IN</div>
+      <div className="sign-up-btn">SIGN UP</div>
+    </header>
+  );
+};
+
+export default Header;
